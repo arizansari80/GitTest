880ad68485ca82691fe19294ba03e4ecbfafbea9
Mohd Ariz Ansari <arizansari80@gmail.com>
2017-08-03 20:17:13 +0530

    First Commit by

---
+++ Test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+#define endl printf("\n")
+
+int main(int argc,char **argv){
+	
+	return 0;
+}
